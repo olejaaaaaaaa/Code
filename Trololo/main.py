3350400be538c129bb8c1a7f92fa80ceed053f63
olejaaaaaaaa <k09901173@gmail.com>
2024-03-08 14:45:36 +0300

    2 challenge with Codewars

---
+++ Trololo/main.py
@@ -0,0 +1,33 @@
+from typing import List
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(disemvowel("This is for losers!"), "Ths s fr lsrs!")
+        self.assertEqual(disemvowel("LOL"), "LL")
+
+
+
+def disemvowel(string_: str):
+    
+    res: str = ""
+    letters: List[str] = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
+
+    for i in string_:
+
+        b = False
+        for j in letters:
+               
+            if i == j:
+                b = True
+
+        if not b:
+            res += i        
+
+        
+    return res
+
+if __name__ == "__main__":
+    unittest.main()
